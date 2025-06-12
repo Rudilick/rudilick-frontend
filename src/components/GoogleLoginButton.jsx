d08cf7687ec7b69e950425b28e39463d639f769e
Rudilick <dnffpr2@gmail.com>
2025-06-12 23:07:08 +0900

    홈탭 영상위에 구글로그인 타일을 올려놓으려고했는데 하얗게만 떠서 수정

--- src/components/GoogleLoginButton.jsx
+++ src/components/GoogleLoginButton.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useGoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 import { useUser } from '../contexts/UserContext';
 
 const CLIENT_ID = "170520257716-ftoecfa6d0pd19453hc41fooaanrh9nd.apps.googleusercontent.com";
@@ -7,30 +7,21 @@ const CLIENT_ID = "170520257716-ftoecfa6d0pd19453hc41fooaanrh9nd.apps.googleuser
 export default function GoogleLoginButton() {
   const { login, user } = useUser();
 
-  const googleLogin = useGoogleLogin({
-    onSuccess: (tokenResponse) => {
-      const token = tokenResponse.credential;
-      login(token);
-    },
-    onError: () => {
-      console.error("❌ Google Login Failed");
-    }
-  });
-
   return (
     <GoogleOAuthProvider clientId={CLIENT_ID}>
       <div className="flex justify-center">
         {user ? (
           <div className="text-xs text-gray-400">✅ {user.email}</div>
         ) : (
-          <button
-            onClick={() => googleLogin()}
-            className="bg-white px-4 py-2 rounded-full shadow"
-          >
-            <span className="opacity-30 font-medium text-sm">
-              Google 계정으로 로그인
-            </span>
-          </button>
+          <GoogleLogin
+            onSuccess={(res) => {
+              const token = res.credential;
+              login(token);
+            }}
+            onError={() => {
+              console.error("❌ Google Login Failed");
+            }}
+          />
         )}
       </div>
     </GoogleOAuthProvider>
