8f1c7051af0169bf395f160b527539747a278ae2
Rudilick <dnffpr2@gmail.com>
2025-06-13 08:04:52 +0900

    구글로그인버튼을 투명하게 2

--- src/components/GoogleLoginButton.jsx
+++ src/components/GoogleLoginButton.jsx
@@ -5,30 +5,30 @@ import { useUser } from '../contexts/UserContext';
 const CLIENT_ID = '170520257716-ftoecfa6d0pd19453hc41fooaanrh9nd.apps.googleusercontent.com';
 
 export default function GoogleLoginButton() {
-	const { login, user } = useUser();
+  const { login, user } = useUser();
 
-	return (
-		<GoogleOAuthProvider clientId={CLIENT_ID}>
-			<div className="flex justify-center">
-				{user ? (
-					<div className="text-xs text-gray-400">✅ {user.email}</div>
-				) : (
-					<div className="bg-transparent rounded-full shadow-none">
-						<GoogleLogin
-							onSuccess={(credentialResponse) => {
-								const token = credentialResponse.credential;
-								login(token);
-							}}
-							onError={() => {
-								console.error('❌ Google Login Failed');
-							}}
-							theme="filled_black"
-							text="signin_with"
-							shape="pill"
-						/>
-					</div>
-				)}
-			</div>
-		</GoogleOAuthProvider>
-	);
+  return (
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+      <div className="flex justify-center">
+        {user ? (
+          <div className="text-xs text-gray-400">✅ {user.email}</div>
+        ) : (
+          <div className="bg-black/50 px-3 py-2 rounded-full backdrop-blur-sm">
+            <GoogleLogin
+              onSuccess={(credentialResponse) => {
+                const token = credentialResponse.credential;
+                login(token);
+              }}
+              onError={() => {
+                console.error('❌ Google Login Failed');
+              }}
+              theme="filled_black"
+              text="signin_with"
+              shape="pill"
+            />
+          </div>
+        )}
+      </div>
+    </GoogleOAuthProvider>
+  );
 }
